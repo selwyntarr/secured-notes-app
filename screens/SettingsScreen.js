5da265d23b5ba493fc293a2bc9af59085051fafd
edralyyn <qerperalta@tip.edu.ph>
2023-12-05 02:23:59 +0800

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import {Image, View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Pressable } from 'react-native';
+import {TextInput, Image, View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Pressable } from 'react-native';
 import Colors from '../components/Colors';
+import { useState } from 'react';
 
 const SettingsScreen = ({ navigation }) => {
 
-  const handleAddRecord = () => {
-    console.log('Logout')
+  const [fullname, setFullname] = useState('John Doe');
+  const [email, setEmail] = useState('john.doe@example.com');
+  const [username, setUsername] = useState('johndoe');
+  const [password, setPassword] = useState('securepassword');
+  const [confirmpassword, setConfirmPassword] = useState('securepassword');
+
+  const handleLogout = () => {
     navigation.replace('Auth')
+  }
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!isPasswordVisible);
+  }
+  const [isPasswordVisible2, setPasswordVisible2] = useState(false);
+
+  const togglePasswordVisibility2 = () => {
+    setPasswordVisible2(!isPasswordVisible2);
+  }
+  
+  const [confirmVisible, setconfirmVisible] = useState(false);
+
+  const handleEdit = () => {
+    setconfirmVisible(!confirmVisible)
   }
 
   return (
     <SafeAreaView style={styles.container}>
+
       <View style={styles.header}>
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Text style={styles.headerText}>Settings</Text>
-          <Pressable style={styles.addBtn} onPress={handleAddRecord}>
+          <Text style={styles.headerText}>Search</Text>
+          <Pressable style={styles.addBtn} onPress={handleLogout}>
 
             <Text style={{fontSize: 12}}>Logout</Text>
 
@@ -25,34 +48,61 @@ const SettingsScreen = ({ navigation }) => {
         </View>
       </View>
 
-      <View style={styles.user_icon}>
-        <Image source={require('../assets/Display-Picture.png')} resizeMode="contain"
-          style={{height: '100%'}}/>
-      </View>
-
-      <View style={{flexDirection: 'row', alignSelf: 'center', gap: 10}}>
-        <Text style={{fontSize: 30, fontWeight:'bold'}}>John Doe</Text>
-        <Image source={require('../assets/Edit.png')} resizeMode='contain'
-        style={{height: 30, width: 30, alignSelf: 'center'}}/>
-      </View>
-
-      <View style={styles.card}>
-        <View style ={styles.row}>
-          <Text style={{fontSize: 14, fontWeight: 'bold'}}>Account Settings</Text>
+        <View style={styles.user_icon}>
+          <Image source={require('../assets/Display-Picture.png')} resizeMode="contain"
+            style={{height: '100%'}}/>
         </View>
 
-        <View style ={styles.row}>
-          <Text style={{fontSize: 14, fontWeight: 'bold'}}>Preferences</Text>
+        <View style={styles.card}>
+
+          <View style={styles.input_box}>
+            <TextInput value = {fullname} placeholder="Full Name" multiline={false} style={styles.input}/>
+          </View>
+
+          <View style={styles.input_box}>
+            <TextInput value = {email} placeholder="Email" multiline={false} style={styles.input}/>
+          </View>
+
+          <View style={styles.input_box}>
+            <TextInput value = {username} placeholder="Username" multiline={false} style={styles.input}/>
+          </View>
+
+          <View style={{... styles.input_box, flexDirection: 'row', 
+                            justifyContent: 'space-between', gap: 10}}>
+            <TextInput value = {password} placeholder="Password" secureTextEntry={!isPasswordVisible} 
+              multiline={false} style={{...styles.input, flex: 1}}/>
+            <TouchableOpacity
+              onPress={togglePasswordVisibility}
+            >
+              <Image source={isPasswordVisible ? require('../assets/not_visible.png') : require('../assets/visible.png')}
+              style={{height: 25, width: 25}} resizeMode="contain"/>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{... styles.input_box, flexDirection: 'row', 
+                            justifyContent: 'space-between', gap: 10, 
+                            display: confirmVisible? null : 'none'}}>
+            <TextInput value = {confirmpassword} placeholder="Confirm Password" secureTextEntry={!isPasswordVisible2} 
+              multiline={false} style={{...styles.input, flex: 1}}/>
+            <TouchableOpacity
+              onPress={togglePasswordVisibility2}
+            >
+              <Image source={isPasswordVisible2 ? require('../assets/not_visible.png') : require('../assets/visible.png')}
+              style={{height: 25, width: 25}} resizeMode="contain"/>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{alignItems: "center"}}>
+            <Pressable style={styles.btn} onPress={handleEdit}>
+              <Text style={{color: Colors.white}}>{confirmVisible? 'Save Info' : 'Edit Info'}</Text>
+            </Pressable>
+          </View>
+        
         </View>
 
-        <View style ={styles.row}>
-          <Text style={{fontSize: 14, fontWeight: 'bold'}}>About</Text>
-        </View>
-
-      </View>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -60,30 +110,6 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     justifyContent: 'space-between',
     gap: 10
-  },
-  row: {
-    backgroundColor: '#E9E9E9',
-    padding: 13,
-    borderRadius: 15
-  },
-  card: {
-    backgroundColor: Colors.card_container,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    height: '65%',
-    width: '90%',
-    alignSelf: 'center',
-    shadowOffset: { width: -2, height: 1 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 3,
-    padding: 20,
-    flex: 1,
-    gap: 10
-  },
-  user_icon: {
-    height: '20%',
-    alignSelf: "center"
   },
   addBtn:{
     backgroundColor: '#E9E9E9',
@@ -95,6 +121,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignSelf: 'flex-end',
     alignItems: 'center'
+  },
+  btn:{
+    backgroundColor: Colors.dark_brown,
+    padding: 15,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    width: '100%'
   },
   header: {
     backgroundColor: Colors.khaki_30,
@@ -108,9 +142,36 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
   headerText: {
-    alignSelf: 'flex-end',
+    alignSelf: 'flex-start',
     fontSize: 25,
   },
+  card: {
+    backgroundColor: Colors.card_container,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: '65%',
+    width: '90%',
+    alignSelf: 'center',
+    shadowOffset: { width: -2, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 3,
+    padding: 20,
+    flex: 1,
+    gap: 15
+  },
+  user_icon: {
+    height: '20%',
+    alignSelf: "center"
+  },
+  input_box: {
+    backgroundColor: Colors.input_gray,
+    borderRadius: 20,
+    padding: 10
+  },
+  input: {
+    textAlign: "center",
+  }
 });
 
 export default SettingsScreen;
